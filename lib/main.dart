4561a9b0901eeae653c5222e3ec5e7964b987a04
YashSomalkar <yashsomalkar@gmail.com>
2023-03-06 17:46:50 +0530

    screen specific rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:blt/src/app.dart';
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'package:flutter/services.dart';
 
 // Application's entry point
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Check the device's screen size
+  var screenSize =
+      MediaQueryData.fromWindow(WidgetsBinding.instance.window).size;
+  if (screenSize.shortestSide < 600) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]).then((value) => runApp(BLT()));
+  }
   runApp(BLT());
 }
